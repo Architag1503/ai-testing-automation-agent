ff39ccc64c80f5a8bd995129d892e42e3c3d98bf
unknown <architag1503@gmail.com>
2026-06-03 18:19:55 +0530

    Workspace Layout

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+// Define protected routes
+const isProtectedRoute = createRouteMatcher(['/workspace(.*)'])
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  // Protect routes and redirect unauthenticated users
+  if (isProtectedRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html|css|js|gif|svg|jpg|jpeg|png|woff|woff2|ico|csv|docx|xlsx|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    //Always run for clerk-specefic frontent api
+    '/_clerk(.*)',
   ],
-};
+}
